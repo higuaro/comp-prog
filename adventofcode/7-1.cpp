376bb0082c67badd4177228e40fd7c88ebbf87fe
higuaro <henry.eduardo@gmail.com>
2019-04-21 22:32:09 +1000

    Finally solved day 7-1 of Advent of Code

--- adventofcode/7-1.cpp
+++ adventofcode/7-1.cpp
@@ -1,25 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T, size_t N, size_t M = N>
+
+template<typename T, int N, int M>
 using table = array<array<T, M>, N>;
 
-typedef int8_t i8;
-table<i8, 26> g{0};
-void dfs(const i8 n, array<i8, 26>& v, stack<i8>& s) {
-  ;
-}
-int main() {
+typedef priority_queue<int, vector<int>, greater<int>> heap;
 
-  for (size_t i = 0; i < 26; i++) {
-    for (size_t j = 0; j < 26; j++) {
-      g[i][j] = 0;
+template<int N>
+void visit(const table<bool, N, N>& graph, int node,
+    heap& to_visit, map<int, int>& in_degree) {
+  for (int n = 0; n < N; n++)
+    if (n != node && graph[node][n]) {
+      in_degree[n]--;
+      if (!in_degree[n])
+        to_visit.push(n);
     }
-  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  const int N = 26;
+  table<bool, N, N> graph {false};
+
+  map<int, int> in_degree;
+
+  set<int> nodes;
+
   string line;
   while (getline(cin, line, '\n')) {
-    g[line[6] - '0'][line[37] - '0'] = 1;
-  }
-  
-  return 0;
+    // pos 8 in vim
+    // |    +-- pos 13 in vim         pos 44 in vim
+    // |    |                              |
+    // v    v                              v
+    // Step F must be finished before step E can begin.
+    char s = line[13 - 8] - 'A';
+    char d = line[44 - 8] - 'A';
 
+    in_degree[d]++;
+    graph[s][d] = true;
+
+    nodes.insert(s);
+    nodes.insert(d);
+  }
+
+  set<int> origins;
+  for (auto node : nodes) {
+    bool is_origin = true;
+    for (int j = 0; j < N; j++)
+      if (graph[j][node]) {
+        is_origin = false;
+        break;
+      }
+    if (is_origin)
+      origins.insert(node);
+  }
+
+  heap to_visit{greater<int>()};
+  for (int origin : origins)
+    to_visit.push(origin);
+
+  while (!to_visit.empty()) {
+    int node = to_visit.top();
+    to_visit.pop();
+    visit<N>(graph, node, to_visit, in_degree);
+    cout << static_cast<char>(node + 'A');
+  }
+  cout << '\n';
+
+  return 0;
 }
