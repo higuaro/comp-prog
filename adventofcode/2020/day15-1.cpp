e39c4d0defa327d2ffda2786fc8c4666426e2f7e
higuaro <henry.eduardo@gmail.com>
2020-12-15 23:02:27 +1100

    Solved aoc day 15

---
+++ adventofcode/2020/day15-1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  vector<int> start;
+  string token;
+  while (getline(cin, token, ','))
+    start.push_back(stoi(token));
+
+  int turn = 1;
+  int last, spoke;
+  unordered_map<int, pair<int, int>> spoken;
+  for (int i = 0; i < start.size(); i++, turn++) {
+    spoke = start[i];
+    spoken[spoke] = {-1, turn};
+    last = spoke;
+  }
+  while (turn <= 2020) {
+    auto [first, second] = spoken[last];
+    spoke = first == -1 ? 0 : second - first;
+    if (spoken.count(spoke)) {
+      auto n = spoken[spoke];
+      spoken[spoke] = {n.second, turn};
+    } else {
+      spoken[spoke] = {-1, turn};
+    }
+    last = spoke;
+    turn++;
+  }
+  cout << last << endl;
+  return 0;
+}
