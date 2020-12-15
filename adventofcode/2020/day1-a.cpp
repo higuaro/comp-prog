8956e9ccf1445f3ecf29411abbacbb594ed19f90
Henry Iguaro <henry.eduardo@gmail.com>
2020-12-15 23:11:26 +1100

    Update day1-a.cpp

--- adventofcode/2020/day1-a.cpp
+++ adventofcode/2020/day1-a.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
   unordered_set<int> m;
   int n;
   while (cin >> n) 
     m.insert(n);
-  for (auto v : m) {
+  for (auto v : m)
     if (m.count(2020 - v)) {
       cout << v * (2020 - v) << endl;
       return 0;
     }
-  }
   return 0;
 }
