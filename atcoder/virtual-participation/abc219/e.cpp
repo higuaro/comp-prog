5c0963c00fcbe233a27d9ab780f6870959f687d5
higuaro <henry.eduardo@gmail.com>
2021-10-03 22:42:13 +1100

    wip

--- atcoder/virtual-participation/abc219/e.cpp
+++ atcoder/virtual-participation/abc219/e.cpp
@@ -1,37 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool is_valid(int m) {
-  int mat[4][4];
-  for (int i = 0; i < 4; i++)
-    for (int j = 0; j < 4; j++)
-      mat[i][j] = (m >> (15 - i * 4 + j)) & 1;
 
-  
-  cout << "-------- " << bitset<16>(m) << endl;
+void d(int mat[4][4], int m) {
+  cout << "------- " << bitset<16>(m) << endl;
   for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      cout << mat[i][j] << ' ';
-    }
+    for (int j = 0; j < 4; j++) 
+      cout << mat[i][j];
     cout << endl;
   }
-  cout << "--------" << endl;
+}
 
+int bfs_count(queue<pair<int, int>>& q, int mat[4][4]) {
+  set<pair<int, int>> seen;
   int o[5] = {-1, 0, 1, 0, -1};
+  auto [i, j] = q.front();
+  int x = mat[i][j];
+  while (!q.empty()) {
+    auto [i, j] = q.front(); q.pop();
+    seen.insert({i, j});
+    for (int k = 0; k < 4; k++) {
+      int ii = i + o[k];
+      int jj = j + o[k + 1];
+      if (0 <= ii && ii < 4 && 0 <= jj && jj < 4 
+          && mat[ii][jj] == x 
+          && !seen.count({ii, jj}))
+        q.push({ii, jj});
+    }
+  }
+  return seen.size();
+}
+
+bool is_valid(int m, int bits) {
+  int mat[4][4];
+  int m_bits = 0;
+  queue<pair<int, int>> q1;
+  queue<pair<int, int>> q0;
   for (int i = 0; i < 4; i++)
     for (int j = 0; j < 4; j++) {
-      if (mat[i][j]) {
-        int neighbours = 0;
-        for (int k = 0; k < 4; k++) {
-          int ii = i + o[k];
-          int jj = j + o[k + 1];
-          if (0 <= ii && ii < 4 && 0 <= jj && jj < 4)
-            if (mat[ii][jj]) neighbours++;
-        }
-        if (!neighbours) return false;
-      }
+      mat[i][j] = (m >> ((3 - i) * 4 + 3 - j)) & 1;
+      if (mat[i][j]) m_bits++;
+      if (mat[i][j] && q1.empty()) q1.push({i, j});
+      if (!mat[i][j] && q0.empty()) q0.push({i, j});
     }
-  return true;
+
+  bool v = bfs_count(q1, mat) == m_bits && bfs_count(q0, mat) == 16 - m_bits;
+
+if (v) d(mat, m);
+
+  return v;
 }
+
 int main() {
   int m = 0;
   for (int i = 0; i < 16; i++) {
@@ -41,8 +59,8 @@ int main() {
   int bits = __builtin_popcount(m);
   int t = 0;
   for (int k = 0; k < 65535; k++) {
-    if (!(m & k)) continue;
-    if (bits == 1 || is_valid(k)) t++;
+    if (__builtin_popcount(m & k) != bits) continue;
+    if (bits == 1 || is_valid(k, bits)) t++;
   }
   cout << t << endl;
 }
--- cf/rounds/cfr-745/b.cpp
+++ cf/rounds/cfr-745/b.cpp
@@ -4,17 +4,15 @@ using namespace std;
 int main() {
   int t; cin >> t;
   while (t--) {
-    uint64_t n, m, k; cin >> n >> m >> k;
+    int64_t n, m, k; cin >> n >> m >> k;
+    k--;
     uint64_t m_min = n - 1;
-    uint64_t m_max = n * (n - 1) / 2; 
+    uint64_t m_max = n * (n - 1) / 2;
     if (m < m_min || m_max < m) {
       cout << "NO" << endl;
-    } else if (m > n + 1 && k >= 2) {
-      cout << "YES" << endl;
-    } else if (k - 1 <= 2 && m) {
-      cout << "NO" << endl;
     } else {
-      cout << "YES" << endl;
+      int diam = m && m == m_max ? 1 : (!m ? 0 : 2);
+      cout << (diam < k ? "YES" : "NO") << endl;
     }
   }
   return 0;
