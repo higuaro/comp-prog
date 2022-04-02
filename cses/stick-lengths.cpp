d55af796da1456fa35ec04ea8ec577c3efa38551
higuaro <henry.eduardo@gmail.com>
2022-04-02 14:01:28 +1100

    solved stick lengths

--- cses/stick-lengths.cpp
+++ cses/stick-lengths.cpp
@@ -1,17 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <cmath>
-#include <unordered_set>
+#include <bits/stdc++.h>
 using namespace std;
 typedef int64_t ll;
 const ll oo = numeric_limits<ll>::max();
 ll cost(const int n, const vector<ll>& p, const vector<ll>& s, int i) {
   ll sl = i > 0 ? s[i - 1] : 0;
   ll sr = i < n - 1 ? s[n - 1] - s[i] : 0;
-  ll il = p[i] * (i > 0 ? i - 1 : 0);
-  ll ir = p[i] * (i < n - 1 ? n - i : 0);
+  ll il = p[i] * i;
+  ll ir = p[i] * (n - i - 1);
   return abs(sl - il) + abs(sr - ir);
 }
 int main() {
@@ -22,16 +17,12 @@ int main() {
   partial_sum(begin(p), end(p), begin(sum));
 
   unordered_set<ll> seen;
-  ll best_cost = oo, best_p = 0;
+  ll best = oo;
   for (int i = 0; i < n; i++) {
     if (seen.count(p[i])) continue;
-    ll c = cost(n, p, sum, i);
-    if (c < best_cost) {
-      best_cost = c;
-      best_p =p[i];
-    }
+    best = min(best, cost(n, p, sum, i));
     seen.insert(p[i]);
   }
-  cout << best_p << '\n';
+  cout << best << '\n';
   return 0;
 }
