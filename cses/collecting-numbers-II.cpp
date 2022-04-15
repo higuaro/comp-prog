29f8b6ef8b514e1b21308558a56ccec03ad1c361
higuaro <henry.eduardo@gmail.com>
2022-04-15 23:09:43 +1000

    finally solved collecting-numbers-II

--- cses/collecting-numbers-II.cpp
+++ cses/collecting-numbers-II.cpp
@@ -10,7 +10,7 @@ int runs(const vector<int>& x) {
   return r;
 }
 
-int change(unordered_map<int, int>& pos, vector<int>& x, int a, int b) {
+int change(vector<int>& pos, vector<int>& x, int a, int b) {
   int xa = x[a], xb = x[b];
   set<int> nums{xa - 1, xa, xa + 1, xb - 1, xb, xb + 1};
   nums.erase(0);
@@ -19,22 +19,22 @@ int change(unordered_map<int, int>& pos, vector<int>& x, int a, int b) {
   vector<int> xs(begin(nums), end(nums));
   auto cmp = [&pos](const int n1, const int n2) { return pos[n1] < pos[n2]; };
   sort(begin(xs), end(xs), cmp);
-cout << "before xs: "; for (auto xi : xs) cout << xi << ','; cout << '\n';
+// cout << "before xs: "; for (auto xi : xs) cout << xi << ','; cout << '\n';
   int r1 = runs(xs);
 
   swap(x[a], x[b]);
   pos[xa] = b; pos[xb] = a;
 
   sort(begin(xs), end(xs), cmp);
-cout << " after xs: "; for (auto xi : xs) cout << xi << ','; cout << '\n';
+// cout << " after xs: "; for (auto xi : xs) cout << xi << ','; cout << '\n';
   int r2 = runs(xs);
-cout << "delta: " << r2 - r1 << '\n';
+// cout << "delta: " << r2 - r1 << '\n';
   return r2 - r1;
 }
 
 int main() {
-  unordered_map<int, int> pos;
   int n, m; cin >> n >> m;
+  vector<int> pos(n, 0);
   vector<int> x(n, 0);
   for (int i = 0; i < n; i++) {
     int xi; cin >> xi;
