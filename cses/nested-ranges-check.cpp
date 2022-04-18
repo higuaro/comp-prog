918cb99a290fd6b67f71b0527ddb3eb03c663461
higuaro <henry.eduardo@gmail.com>
2022-04-18 16:31:11 +1000

    cses nested ranges check

--- cses/nested-ranges-check.cpp
+++ cses/nested-ranges-check.cpp
@@ -1,23 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef tuple<int, bool, int> event_t;
+const int oo = numeric_limits<int>::max();
+typedef tuple<int, int, int> seg_t;
 int main() {
   int n; cin >> n;
   vector<bool> contains(n, false);
-  vector<bool> contained(n, false);
-  vector<event_t> events;
+  vector<bool> is_contained(n, false);
+  vector<seg_t> segs;
   for (int i = 0; i < n; i++) {
     int a, b; cin >> a >> b;
-    events.push_back({a, i, true});
-    events.push_back({b, i, false}); 
+    segs.push_back({a, b, i});
   }
-  sort(begin(events), end(events));
-
-  set<int> active_segs;
-  for (auto [xi, open, i] : events) {
-    if (open) 
-      active_segs.insert(i);
-    
+  auto cmp1 = [](const seg_t& p1, const seg_t& p2) {
+    auto [a1, b1, i1] = p1;
+    auto [a2, b2, i2] = p2;
+    return a1 != a2 ? a1 < a2 : b1 > b2;
+  };
+  sort(begin(segs), end(segs), cmp1);
+  int max_r = 0;
+  for (const auto& [l, r, i] : segs) {
+    if (r <= max_r)
+      is_contained[i] = true;
+    max_r = max(max_r, r);
   }
+  auto cmp2 = [](const seg_t& p1, const seg_t& p2) {
+    auto [a1, b1, i1] = p1;
+    auto [a2, b2, i2] = p2;
+    return a1 != a2 ? a1 > a2 : b1 < b2;
+  };
+  sort(begin(segs), end(segs), cmp2);
+  int min_r = oo;
+  for (const auto& [l, r, i] : segs) {
+    if (r >= min_r) 
+      contains[i] = true;
+    min_r = min(min_r, r);
+  }
+  for (auto c : contains)
+    cout << c << ' '; 
+  cout << '\n';
+  for (auto c : is_contained)
+    cout << c << ' '; 
+  cout << '\n';
   return 0;
 }
